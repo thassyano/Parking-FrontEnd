552dc0b422c884498eb0845681879297e8d1a07b
Thassyano <Thassyano19@gmail.com>
2026-03-11 01:16:04 -0300

    front adjustment

--- src/app/pages/public/reserva-online/reserva-online.ts
+++ src/app/pages/public/reserva-online/reserva-online.ts
@@ -14,6 +14,9 @@ export class ReservaOnline {
   cpfCliente = '';
   tipoVaga = 'Coberta';
   dataEntrada = '';
+  horaEntrada = '';
+  dataSaida = '';
+  horaSaida = '';
   qtdDias = 1;
   observacoes = '';
   loading = signal(false);
@@ -25,6 +28,10 @@ export class ReservaOnline {
   ) {
     const hoje = new Date();
     this.dataEntrada = hoje.toISOString().split('T')[0];
+    this.horaEntrada = hoje.toTimeString().slice(0, 5);
+    const saida = new Date(hoje.getTime() + 86400000);
+    this.dataSaida = saida.toISOString().split('T')[0];
+    this.horaSaida = this.horaEntrada;
   }
 
   submeter() {
@@ -42,7 +49,8 @@ export class ReservaOnline {
         telefoneCliente: this.telefoneCliente,
         cpfCliente: this.cpfCliente || undefined,
         tipoVaga: this.tipoVaga,
-        dataEntrada: this.dataEntrada,
+        dataEntrada: `${this.dataEntrada}T${this.horaEntrada || '00:00'}`,
+        dataSaidaPrevista: `${this.dataSaida}T${this.horaSaida || '00:00'}`,
         qtdDias: this.qtdDias,
         observacoes: this.observacoes || undefined,
       })

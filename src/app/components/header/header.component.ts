accf6ae80fc15b2a605e23a29e2488555222d5eb
Rafael Vazquez <rafael.vazquez@cwi.com.br>
2026-05-24 16:55:39 -0300

    feat: adiciona navegação dinâmica se for admin ou cliente pelo título do header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,6 +1,7 @@
 import { Component, inject, input, signal } from '@angular/core';
 import { BackBtnComponent } from '../buttons/back-btn/back-btn.component';
 import { Router } from '@angular/router';
+import { AuthService } from '../../core/services/auth/auth.service';
 
 @Component({
   selector: 'app-header',
@@ -10,10 +11,28 @@ import { Router } from '@angular/router';
 })
 export class HeaderComponent {
   private router = inject(Router);
+  private readonly authService = inject(AuthService);
 
   public headerName = input.required<string>();
 
   protected navigateHome(): void {
+    const header = this.headerName().toLowerCase();
+
+    const isAdmin =
+      header.includes('admin') ||
+      header.includes('administrador') ||
+      header.includes('administração');
+
+    if (isAdmin) {
+      if (this.authService.isAuthenticated()) {
+        this.router.navigateByUrl('/admin/dashboard');
+        return;
+      }
+
+      this.router.navigateByUrl('/admin/login');
+      return;
+    }
+
     this.router.navigateByUrl('');
   }
 }
